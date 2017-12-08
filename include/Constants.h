17aa5773216d263fb75270e17abb6d18db43c8b5
xiongkun <835288864@qq.com>
2017-12-08 09:35:29 +0800

    将常量放到一个文件中

---
+++ include/Constants.h
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////////////////////////////
+// Constants.h
+//////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+// constant data
+// window setting
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const char* WINDOW_CAPTION = "GameName";
+
+// game setting
+const unsigned int FRAMES_PER_SECOND = 30;
+const unsigned int FRAME_RATE = 1000/FRAMES_PER_SECOND;
--- src/Main.cpp
+++ src/Main.cpp
@@ -12,10 +12,45 @@
 #include <cstdio>
 #include <ctime>
 
-#include "../include/Game.h"
+#include <stack>
+
+#include "../include/Constants.h"
 #include "../include/Tools.h"
 
 using namespace std;
+
+// game state struct
+struct StateStruct{
+	void(*StatePointer)();
+};
+
+// global data
+std::stack<StateStruct> gStageStack; // stack for game state pointer
+SDL_Window* gWindow = NULL; // SDL window pointer
+SDL_Renderer* gRenderer = NULL; // renderer pointer
+SDL_Event gEvent; // SDL event struct
+int gTimer; // timer
+LTexture gTextTexture;// texture for text
+
+// functions
+// init and close SDL, load media
+bool initSDL();
+bool loadMedia();
+void closeSDL();
+
+// init and shutdown game
+void init();
+void shutdown();
+
+// functions to handle states of the game
+void Menu();
+void Game();
+void Exit();
+
+// helper functions
+void handleMenuInput();
+void handleGameInput();
+void handleExitInput();
 
 int main(int argc, char** argv) {
 
